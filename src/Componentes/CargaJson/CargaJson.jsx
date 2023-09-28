400b98b9f655ed291578d8ba6f80e8823c1659a2
Galbfran <francogalbiati984@gmail.com>
2023-09-28 16:38:41 -0300

    subo carga de de json a admin

--- src/Componentes/CargaJson/CargaJson.jsx
+++ src/Componentes/CargaJson/CargaJson.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -11,21 +11,32 @@ const CargaJson = () => {
 
   const handleFileUpload = async () => {
     if (selectedFile) {
-      const formData = new FormData();
-      formData.append('file', selectedFile);
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        const fileContent = event.target.result;
 
-      try {
-        // Envía el archivo al servidor (puedes usar la ruta correcta para tu API)
-        const response = await axios.post('/api/encuesta', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
+        try {
+          // Parsea el contenido del archivo como JSON
+          const parsedData = JSON.parse(fileContent);
 
-        console.log('Archivo cargado exitosamente:', response.data);
-      } catch (error) {
-        console.error('Error al cargar el archivo:', error);
-      }
+          // Aquí puedes realizar cualquier operación con parsedData
+          console.log('Datos parseados del archivo:', parsedData);
+
+          // Ahora, envía parsedData al servidor (puedes usar la ruta correcta para tu API)
+          const response = await axios.post('/api/encuesta', parsedData, {
+            headers: {
+              'Content-Type': 'application/json', // Define el tipo de contenido como JSON
+            },
+          });
+
+          console.log('Archivo cargado exitosamente:', response.data);
+        } catch (error) {
+          console.error('Error al cargar o parsear el archivo:', error);
+        }
+      };
+
+      // Lee el contenido del archivo como texto
+      reader.readAsText(selectedFile);
     } else {
       console.error('No se ha seleccionado ningún archivo.');
     }
